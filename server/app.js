7e30c26328316006a8487a0d6b9009060004732b
ajunc <894012405@qq.com>
2020-05-03 16:43:58 +0800

    router test

--- server/app.js
+++ server/app.js
@@ -1,2 +1,12 @@
-import koa from 'koa'
-export default koa
+import Koa from 'koa'
+import Router from 'Koa-router'
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/',async ctx => {
+    ctx.body = 'hello world!'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+app.listen(3000)
